4deae83895c05d7d0d99053d1525fb35f1f8cabd
sumedh <ckurundkar@yahoo.co.in>
2020-06-09 01:04:17 +0530

    Fetching JSON data from the REST API

--- app/src/main/java/com/pheonix/yujapplication/MainActivity.java
+++ app/src/main/java/com/pheonix/yujapplication/MainActivity.java
@@ -3,12 +3,71 @@ package com.pheonix.yujapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+        mQueue = Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
     }
+
+    private void jsonParse() {
+        String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+                                int id = employee.getInt("id");
+                                String firstName = employee.getString("employee_name");
+                                int salary = employee.getInt("employee_salary");
+                                int age = employee.getInt("employee_age");
+                                String profile_image = employee.getString("profile_image");
+                                mTextViewResult.append(String.valueOf(id) + ", " + firstName + ", "+ String.valueOf(salary) + ", " + String.valueOf(age)+ ", "+ profile_image + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
 }
